af6f0ad7dde5527b840fda6a6db7c81602db12a3
hongnhung1986 <32408036+hongnhung1986@users.noreply.github.com>
2019-03-28 21:07:28 +0700

    BaiTap 1+2

---
+++ face detection.py
@@ -0,0 +1,18 @@
+import numpy as np
+import cv2 as cv
+
+face_cascade = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
+img = cv.imread('gia_dinh.jpg')
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+for (x,y,w,h) in faces:
+    cv.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+    roi_gray = gray[y:y+h, x:x+w]
+    roi_color = img[y:y+h, x:x+w]
+cv.imshow('img',img)
+
+img_detect='gia_dinh_detect.jpg'
+cv.imwrite(img_detect,img)
+cv.waitKey(0)
+cv.destroyAllWindows()
